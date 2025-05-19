6e6c6661067d0c3302e4cfb9273c6501085c588b
pgm-casverheye <cas.verheye@student.arteveldehs.be>
2025-05-19 18:02:35 +0200

    paginated-backlog

---
+++ client/Jammin/src/Backlog.jsx
@@ -0,0 +1,22 @@
+export default function Backlog({ tasks }) {
+  return (
+    <table className="table is-fullwidth">
+      <thead>
+        <tr>
+          <th className="title pl-3">Title</th>
+          <th className="title pl-3">Description</th>
+          <th className="title pl-3">Status</th>
+        </tr>
+      </thead>
+      <tbody>
+        {tasks.map((task) => (
+          <tr key={task.id}>
+            <td className="is-family-code	">{task.title}</td>
+            <td className="is-family-code	">{task.description}</td>
+            <td className="is-family-code	">{task.taskStatus?.name}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+}
---
+++ client/Jammin/src/PaginatedBacklog.jsx
@@ -0,0 +1,47 @@
+import { useState } from 'react'
+import { useQuery } from '@tanstack/react-query'
+import Backlog from './Backlog'
+import Pagination from './Pagination'
+
+const PAGE_SIZE_OPTIONS = [1, 2, 5, 10];
+
+function fetchTasks(page, pageSize) {
+  return fetch(
+    `http://localhost:1337/api/tasks?populate=taskStatus&filters[taskStatus][name][$eq]=Backlog&pagination[page]=${page}&pagination[pageSize]=${pageSize}`
+  ).then(res => res.json())
+}
+
+export default function PaginatedBacklog() {
+  const [page, setPage] = useState(1)
+  const [pageSize, setPageSize] = useState(PAGE_SIZE_OPTIONS[2])
+
+  const { data, isLoading, isError } = useQuery({
+    queryKey: ['backlog', page, pageSize],
+    queryFn: () => fetchTasks(page, pageSize),
+    keepPreviousData: true,
+  })
+
+  if (isLoading) return <p>Loading...</p>
+  if (isError) return <p>Error loading backlog.</p>
+
+  const tasks = data?.data || []
+  const pagination = data?.meta?.pagination || {}
+
+  return (
+    <section className='container'>
+      <h2 className='title is-1'>Backlog</h2>
+      <Backlog tasks={tasks} />
+      <Pagination
+        currentPage={pagination.page}
+        pageCount={pagination.pageCount}
+        onPageChanged={setPage}
+        pageSize={pageSize}
+        onPageSizeChange={size => {
+          setPageSize(size)
+          setPage(1)
+        }}
+        pageSizeOptions={PAGE_SIZE_OPTIONS}
+      />
+    </section>
+  );
+}
