60cdbd78c4932251111a44d6ccfc9e094648247b
pgm-casverheye <cas.verheye@student.arteveldehs.be>
2025-05-22 11:47:19 +0200

    Small update

--- client/Jammin/src/constants/constants.js
+++ client/Jammin/src/constants/constants.js
@@ -1,5 +1,5 @@
 export const API_URL = import.meta.env.PROD
-  ? 'https://jammin-api-5qbt.onrender.com'
+  ? 'https://jammin-api-5qbt.onrender.com/api'
   : 'http://localhost:1337/api'
 
 export const API_TOKEN = import.meta.env.PROD
