9ae8f85c384ff4e22685c88fb5e4450b9c08856c
pgm-casverheye <cas.verheye@student.arteveldehs.be>
2025-05-22 11:12:58 +0200

    constants

--- client/Jammin/src/PaginatedBacklog.jsx
+++ client/Jammin/src/PaginatedBacklog.jsx
@@ -2,20 +2,20 @@ import { useState } from 'react'
 import { useQuery } from '@tanstack/react-query'
 import Backlog from './Backlog'
 import Pagination from './Pagination'
+import { API_URL, API_TOKEN } from './constants/constants'
 
 const PAGE_SIZE_OPTIONS = [1, 2, 5, 10];
-const TOKEN = import.meta.env.VITE_REACT_APP_STRAPI_TOKEN;
 
 export default function PaginatedBacklog() {
   const [page, setPage] = useState(1)
   const [pageSize, setPageSize] = useState(PAGE_SIZE_OPTIONS[2])
 
   const fetchTasks = async (page, pageSize) => {
-    const url = `http://localhost:1337/api/tasks?populate=taskStatus&filters[taskStatus][name][$eq]=Backlog&pagination[page]=${page}&pagination[pageSize]=${pageSize}`
+    const url = `${API_URL}/tasks?populate=taskStatus&filters[taskStatus][name][$eq]=Backlog&pagination[page]=${page}&pagination[pageSize]=${pageSize}`
     try {
       const response = await fetch(url, {
         headers: {
-          Authorization: `Bearer ${TOKEN}`,
+          Authorization: `Bearer ${API_TOKEN}`,
         },
       })
       if (!response.ok) throw new Error('Failed to fetch tasks')
